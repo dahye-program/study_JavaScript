f03f08279257ba9d1d24066665f6f51a28fe6cf6
dahye-program <dahye0729@sch.ac.kr>
2021-05-17 22:48:24 +0900

    [Callback] example

--- async/callback.js
+++ async/callback.js
@@ -4,6 +4,7 @@
 // Execute the code block by orger after hoisting. (호이스팅이 된 이후부터 작성한 순서에 맞춰서 실행)
 // hoisting : var, function declaration (var변수와 함수 선언이 제일 위로 올라감)
 
+// 콜백함수 : 이 함수를 나중에 다시 불러줘!!
 console.log('1'); // 동기
 setTimeout(function(){ // 브라우저에게 요청 
     console.log('2'); // 비동기
@@ -22,6 +23,13 @@ function printWithDelay(print, timeout){
     setTimeout(print, timeout);
 }
 printWithDelay(()=>console.log('async callback'),2000); // 비동기
+
+// 출력:
+// 1
+// 3
+// hello
+// 2
+// async callback
 
 // Callback Hell example
 class UserStorage{
